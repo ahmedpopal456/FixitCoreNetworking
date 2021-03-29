12f0697c0d3935b3eb46188c7cd100de4f5f0c8f
Joanna Lin <l.joanna19@hotmail.com>
2021-03-29 23:21:37 +0000

    Merged PR 264: Move over networking files from DataContracts
    Move over networking files from DataContracts
    
    Related work items: #502, #503

---
+++ Fixit.Core.Networking/Extensions/HttpClientCoreExtension.cs
@@ -0,0 +1,38 @@
+﻿using Fixit.Core.Networking.Local.UMS;
+using Fixit.Core.Networking.Local.MDM;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using Fixit.Core.Networking.Local.NMS;
+
+namespace Fixit.Core.Networking.Extensions
+{
+  public static class HttpClientCoreExtension
+  {
+    public static void AddUmServices(this IServiceCollection services, string serviceEndpoint)
+    {
+      services.AddTransient<IFixUmsHttpClient, FixUmsHttpClient>(provider =>
+      {
+          var httpClientCore = new HttpClientCore(new HttpClient(), serviceEndpoint);
+          return new FixUmsHttpClient(httpClientCore);
+      });
+    }
+
+    public static void AddMdmServices(this IServiceCollection services, string serviceEndpoint)
+    {
+      services.AddTransient<IFixMdmHttpClient, FixMdmHttpClient>(provider =>
+      {
+          var httpClientCore = new HttpClientCore(new HttpClient(), serviceEndpoint);
+          return new FixMdmHttpClient(httpClientCore);
+      });
+    }
+
+    public static void AddNmsServices(this IServiceCollection services, string serviceEndpoint)
+    {
+      services.AddTransient<IFixNmsHttpClient, FixNmsHttpClient>(provider =>
+      {
+        var httpClientCore = new HttpClientCore(new HttpClient(), serviceEndpoint);
+        return new FixNmsHttpClient(httpClientCore);
+      });
+    }
+  }
+}
---
+++ Fixit.Core.Networking/Local/NMS/IFixNmsHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Fixit.Core.DataContracts;
+using Fixit.Core.DataContracts.Notifications.Operations;
+
+namespace Fixit.Core.Networking.Local.NMS
+{
+  public interface IFixNmsHttpClient
+  {
+    Task<OperationStatus> PostNotification(EnqueueNotificationRequestDto enqueueNotificationRequestDto, CancellationToken cancellationToken);
+  }
+}
